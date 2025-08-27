193fd31d6368e6cbe5a343069bf3196f4ee84f53
likanug <likanug.g@qq.com>
2025-08-27 18:59:39 +0800

    feat: 应用启动前添加启动异步定时任务

--- ani_subs/src/main.rs
+++ ani_subs/src/main.rs
@@ -5,6 +5,7 @@ use sqlx::postgres::PgPoolOptions;
 use ani_subs::configuration::get_configuration;
 use ani_subs::startup::run;
 use ani_subs::telemetry::{get_subscriber, init_subscriber};
+use ani_subs::timer_tasker::start_async_timer_task;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -14,6 +15,9 @@ async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool =
         PgPoolOptions::new().connect_lazy_with(configuration.database.connect_options());
+    // 启动异步定时任务
+    let task_config = configuration.datasource;
+    start_async_timer_task(task_config["anime"].clone()).await;
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
