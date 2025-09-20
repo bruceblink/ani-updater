4062dbf4d0299c075bc719581ddbd06e009e6633
likanug <likanug.g@qq.com>
2025-09-20 20:25:14 +0800

    refactor: 更新ani_subs中service/task_service.rs中的代码结构

--- ani_subs/src/service/task_service.rs
+++ ani_subs/src/service/task_service.rs
@@ -13,27 +13,6 @@ use timer_tasker::task::build_tasks_from_meta;
 use tokio::sync::mpsc;
 use tracing::warn;
 
-pub async fn run_task_service(
-    ani_item_result: AniItemResult,
-    pool: Arc<PgPool>,
-) -> anyhow::Result<ApiResponse, String> {
-    // 启动定时任务服务
-    let weekday = get_today_weekday().name_cn.to_string();
-
-    let items = match ani_item_result.get(&weekday) {
-        Some(v) if !v.is_empty() => v,
-        Some(_) => return Ok(ApiResponse::ok(json!({ "message": "没有可插入的数据" }))),
-        None => return Ok(ApiResponse::err("获取今日动漫数据失败")),
-    };
-
-    for item in items {
-        if let Err(e) = upsert_ani_info(item, &pool).await {
-            return Ok(ApiResponse::err(format!("插入失败：{e}")));
-        }
-    }
-    Ok(ApiResponse::ok(json!({ "message": "save success" })))
-}
-
 /// 启动异步定时任务
 pub async fn start_async_timer_task(task_metas: Vec<TaskMeta>, connect_pool: PgPool) {
     // 2) 构建命令表（CmdFn 映射）
@@ -70,4 +49,25 @@ pub async fn start_async_timer_task(task_metas: Vec<TaskMeta>, connect_pool: PgP
             scheduler_run.run(tx).await;
         }
     });
+}
+
+pub async fn run_task_service(
+    ani_item_result: AniItemResult,
+    pool: Arc<PgPool>,
+) -> anyhow::Result<ApiResponse, String> {
+    // 启动定时任务服务
+    let weekday = get_today_weekday().name_cn.to_string();
+
+    let items = match ani_item_result.get(&weekday) {
+        Some(v) if !v.is_empty() => v,
+        Some(_) => return Ok(ApiResponse::ok(json!({ "message": "没有可插入的数据" }))),
+        None => return Ok(ApiResponse::err("获取今日动漫数据失败")),
+    };
+
+    for item in items {
+        if let Err(e) = upsert_ani_info(item, &pool).await {
+            return Ok(ApiResponse::err(format!("插入失败：{e}")));
+        }
+    }
+    Ok(ApiResponse::ok(json!({ "message": "save success" })))
 }
