f7d0e9f18c8a2cc4f3d90837e4eabe4a69d72956
likanug <likanug.g@qq.com>
2025-09-20 23:07:03 +0800

    feat: 新增豆瓣爬虫获取数据如果的dao曾实现

---
+++ ani_subs/src/dao/postgresql/video_info_table.rs
@@ -0,0 +1,44 @@
+use anyhow::Result;
+use common::api::VideoItem;
+use sqlx::PgPool;
+
+/// 视频信息插入新记录
+pub async fn upsert_video_info(item: &VideoItem, db_pool: &PgPool) -> Result<()> {
+    let _ = sqlx::query(
+        r#"
+        INSERT INTO public.video_info (
+            title,
+            rating,
+            pic,
+            is_new,
+            uri,
+            episodes_info,
+            card_subtitle,
+            type
+        ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+        ON CONFLICT (title, episodes_info) DO UPDATE SET
+            rating = EXCLUDED.rating,
+            pic = EXCLUDED.pic,
+            is_new = EXCLUDED.is_new,
+            uri = EXCLUDED.uri,
+            episodes_info = EXCLUDED.episodes_info,
+            card_subtitle = EXCLUDED.card_subtitle
+        "#,
+    )
+    .bind(&item.title)
+    .bind(&item.rating)
+    .bind(&item.pic)
+    .bind(item.is_new)
+    .bind(&item.uri)
+    .bind(&item.episodes_info)
+    .bind(&item.card_subtitle)
+    .bind(&item.r#type)
+    .execute(db_pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("插入或更新 video_info {:?} 失败: {}", item, e);
+        anyhow::anyhow!(e)
+    })?;
+
+    Ok(())
+}
