3f43e0c5bd2c0b5b0320434c9a153a02a9a1aeb6
likanug <likanug.g@qq.com>
2025-09-26 19:02:26 +0800

    fix: 修复user_info存在，执行20250902062709_alter_users_table_name.sql失败的问题，增加user_info表不存在的判断

--- migrations/20250902062709_alter_users_table_name.sql
+++ migrations/20250902062709_alter_users_table_name.sql
@@ -1,6 +1,16 @@
 -- Add migration script here
-DO $$ BEGIN
-    IF EXISTS (SELECT 1 FROM pg_tables WHERE schemaname = 'public' AND tablename = 'users') THEN
-        EXECUTE 'ALTER TABLE users RENAME TO user_info';
-    END IF;
-END $$;
+DO $$
+    BEGIN
+        IF EXISTS (
+            SELECT 1
+            FROM pg_tables
+            WHERE schemaname = 'public' AND tablename = 'users'
+        )
+            AND NOT EXISTS (
+                SELECT 1
+                FROM pg_tables
+                WHERE schemaname = 'public' AND tablename = 'user_info'
+            ) THEN
+            EXECUTE 'ALTER TABLE users RENAME TO user_info';
+        END IF;
+    END $$;
