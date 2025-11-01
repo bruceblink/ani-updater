de805dd704c6a8200fa12b042ba33689aac03065
likanug <likanug.g@qq.com>
2025-11-01 16:03:08 +0800

    refactor: API"/api/anis"的返回报文中update_time字段转换时区为东8区

--- ani_subs/src/dao/postgresql/ani_info_table.rs
+++ ani_subs/src/dao/postgresql/ani_info_table.rs
@@ -1,10 +1,12 @@
-use crate::dao::postgresql::run_query;
 use crate::domain::dto::AniInfoDto;
 use crate::domain::po::AniInfo;
+use crate::routes::AniQuery;
+use actix_web::web;
 use anyhow::Result;
+use chrono::Utc;
 use chrono_tz::Asia::Shanghai;
-use common::api::AniItem;
-use sqlx::PgPool;
+use common::api::{AniItem, ApiError, PageData};
+use sqlx::{FromRow, PgPool, Postgres, QueryBuilder};
 
 /// 动漫信息插入新记录
 pub async fn upsert_ani_info(item: &AniItem, db_pool: &PgPool) -> Result<()> {
@@ -74,39 +76,110 @@ pub async fn get_ani_info_by_id(id: i64, db_pool: &PgPool) -> Result<Option<AniI
     Ok(dto)
 }
 
+// 定义包含 AniInfo 和 total_count 的结构体
+#[derive(Debug, FromRow, Clone)]
+struct AniInfoWithTotal {
+    pub id: i64,
+    pub title: String,
+    pub update_count: String,
+    pub update_info: String,
+    pub image_url: String,
+    pub detail_url: String,
+    pub update_time: chrono::DateTime<Utc>,
+    pub platform: String,
+    pub total_count: i64,
+}
+
 /// 查询所有动漫信息
-pub async fn list_all_ani_info(_title: String, db_pool: &PgPool) -> Result<Vec<AniInfoDto>> {
+pub async fn list_all_ani_info(
+    query: web::Query<AniQuery>,
+    db_pool: &PgPool,
+) -> Result<PageData<AniInfoDto>> {
     // 构造带绑定参数的 QueryAs
-    let query = sqlx::query_as::<_, AniInfo>(
-        r#"
-                SELECT id,
-                    title,
-                    update_count,
-                    update_info,
-                    image_url,
-                    detail_url,
-                    update_time,
-                    platform
-                FROM ani_info
-                WHERE 
-                    update_time >= current_date
-                ORDER BY update_time DESC
-            ;"#,
+    let mut result = PageData {
+        items: vec![],
+        total_count: 0,
+        page: 0,
+        page_size: 0,
+        total_pages: 0,
+    };
+
+    let mut query_builder: QueryBuilder<Postgres> = QueryBuilder::new(
+        r#"SELECT id,
+                  title,
+                  update_count,
+                  update_info,
+                  image_url,
+                  detail_url,
+                  update_time,
+                  platform,
+                  COUNT(*) OVER() as total_count
+           FROM ani_info WHERE update_time >= current_date"#,
     );
-    // 调用通用的 run_query
-    let list = run_query(db_pool, query).await?;
-    let list = list
-        .into_iter()
+
+    if let Some(filter) = &query.filter {
+        if let Some(title) = &filter.title {
+            query_builder.push(" AND title LIKE ");
+            query_builder.push_bind(format!("%{title}%"));
+        }
+        if let Some(platform) = &filter.platform {
+            query_builder.push(" AND platform = ");
+            query_builder.push_bind(platform);
+        }
+    }
+
+    query_builder.push(" ORDER BY update_time DESC");
+
+    if let Some(page_size) = query.page_size {
+        query_builder.push(" LIMIT ");
+        query_builder.push_bind(page_size as i64);
+        result.page_size = page_size;
+    }
+
+    if let (Some(page), Some(page_size)) = (query.page, query.page_size) {
+        query_builder.push(" OFFSET ");
+        query_builder.push_bind(((page - 1) * page_size) as i64);
+        result.page = page;
+    }
+    // 查询 数据库的原始数据
+    let rows: Vec<AniInfoWithTotal> = query_builder
+        .build_query_as()
+        .fetch_all(db_pool)
+        .await
+        .map_err(|e| {
+            tracing::error!("数据库查询错误: {e:?}");
+            ApiError::Database("数据库查询失败".into())
+        })?;
+    // 转换数据库数据为前端需要的的DTO数据
+    let data: Vec<AniInfoDto> = rows
+        .iter()
         .map(|ani| AniInfoDto {
             id: ani.id,
-            title: ani.title,
-            update_count: ani.update_count,
-            update_info: ani.update_info,
-            image_url: ani.image_url,
-            detail_url: ani.detail_url,
+            title: ani.title.clone(),
+            update_count: ani.update_count.clone(),
+            update_info: ani.update_info.clone(),
+            image_url: ani.image_url.clone(),
+            detail_url: ani.detail_url.clone(),
             update_time: ani.update_time.with_timezone(&Shanghai).to_rfc3339(),
-            platform: ani.platform,
+            platform: ani.platform.clone(),
         })
-        .collect();
-    Ok(list)
+        .collect::<Vec<AniInfoDto>>();
+
+    result.items = data;
+    let total_count = if rows.is_empty() {
+        0
+    } else {
+        rows[0].total_count
+    };
+    result.total_count = total_count as usize;
+    let total_pages = if total_count == 0 {
+        0
+    } else {
+        query
+            .page_size
+            .map(|ps| ((total_count as f64) / (ps as f64)).ceil() as u32)
+            .unwrap_or(0)
+    };
+    result.total_pages = total_pages;
+    Ok(result)
 }
--- ani_subs/src/routes/api/anis/ani_info.rs
+++ ani_subs/src/routes/api/anis/ani_info.rs
@@ -1,23 +1,22 @@
-use crate::dao::get_ani_info_by_id;
-use crate::domain::po::AniInfo;
+use crate::dao::{get_ani_info_by_id, list_all_ani_info};
 use actix_web::{HttpResponse, web};
-use common::api::{ApiError, ApiResponse, ApiResult, PageData};
+use common::api::{ApiError, ApiResponse, ApiResult};
 use serde::Deserialize;
-use sqlx::{FromRow, PgPool, Postgres, QueryBuilder, Row};
+use sqlx::PgPool;
 
 // 定义嵌套的查询参数结构
 #[derive(Debug, Deserialize, Clone)]
 pub struct Filter {
-    title: Option<String>,
-    platform: Option<String>,
+    pub title: Option<String>,
+    pub platform: Option<String>,
 }
 
 #[derive(Debug, Deserialize, Clone)]
 pub struct AniQuery {
-    filter: Option<Filter>,
+    pub filter: Option<Filter>,
     //sort: Option<String>, // 例如 "price", "-price", "name,-price"
-    page: Option<u32>,
-    page_size: Option<u32>,
+    pub page: Option<u32>,
+    pub page_size: Option<u32>,
 }
 
 pub async fn get_ani(path: web::Path<(i64,)>, pool: web::Data<PgPool>) -> ApiResult {
@@ -33,85 +32,11 @@ pub async fn get_ani(path: web::Path<(i64,)>, pool: web::Data<PgPool>) -> ApiRes
 }
 
 pub async fn get_anis(query: web::Query<AniQuery>, pool: web::Data<PgPool>) -> ApiResult {
-    let mut result = PageData {
-        items: vec![],
-        total_count: 0,
-        page: 0,
-        page_size: 0,
-        total_pages: 0,
-    };
-
-    let mut query_builder: QueryBuilder<Postgres> = QueryBuilder::new(
-        r#"SELECT id,
-                  title,
-                  update_count,
-                  update_info,
-                  image_url,
-                  detail_url,
-                  update_time,
-                  platform,
-                  COUNT(*) OVER() as total_count
-           FROM ani_info WHERE update_time >= current_date"#,
-    );
-
-    if let Some(filter) = &query.filter {
-        if let Some(title) = &filter.title {
-            query_builder.push(" AND title LIKE ");
-            query_builder.push_bind(format!("%{title}%"));
-        }
-        if let Some(platform) = &filter.platform {
-            query_builder.push(" AND platform = ");
-            query_builder.push_bind(platform);
-        }
-    }
-
-    query_builder.push(" ORDER BY update_time DESC");
-
-    if let Some(page_size) = query.page_size {
-        query_builder.push(" LIMIT ");
-        query_builder.push_bind(page_size as i64);
-        result.page_size = page_size;
-    }
-
-    if let (Some(page), Some(page_size)) = (query.page, query.page_size) {
-        query_builder.push(" OFFSET ");
-        query_builder.push_bind(((page - 1) * page_size) as i64);
-        result.page = page;
-    }
-
-    let rows = query_builder
-        .build()
-        .fetch_all(pool.get_ref())
-        .await
-        .map_err(|e| {
+    match list_all_ani_info(query, &pool).await {
+        Ok(page) => Ok(HttpResponse::Ok().json(ApiResponse::ok(page))),
+        Err(e) => {
             tracing::error!("数据库查询错误: {e:?}");
-            ApiError::Database("数据库查询失败".into())
-        })?;
-
-    let data: Vec<AniInfo> = rows
-        .iter()
-        .map(AniInfo::from_row)
-        .collect::<Result<_, _>>()
-        .map_err(|e| {
-            tracing::error!("数据转换错误: {e:?}");
-            ApiError::Database("数据转换失败".into())
-        })?;
-    result.items = data;
-    let total_count = if rows.is_empty() {
-        0
-    } else {
-        rows[0].get::<i64, _>("total_count")
-    };
-    result.total_count = total_count as usize;
-    let total_pages = if total_count == 0 {
-        0
-    } else {
-        query
-            .page_size
-            .map(|ps| ((total_count as f64) / (ps as f64)).ceil() as u32)
-            .unwrap_or(0)
-    };
-    result.total_pages = total_pages;
-    // 返回 JSON 响应
-    Ok(HttpResponse::Ok().json(ApiResponse::ok(result)))
+            Err(ApiError::Database("数据库查询失败".into()))
+        }
+    }
 }
