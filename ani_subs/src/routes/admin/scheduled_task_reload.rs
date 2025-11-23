bf7998ed1443be549e8947503f2d02b9f9af8c82
likanug <likanug.g@qq.com>
2025-11-23 21:19:10 +0800

    refactor: 更新"/admin/task/reload"的API的请求方式为PUT

--- ani_subs/src/routes/admin/scheduled_task_reload.rs
+++ ani_subs/src/routes/admin/scheduled_task_reload.rs
@@ -1,9 +1,9 @@
 use crate::common::AppState;
-use actix_web::{HttpResponse, get, web};
+use actix_web::{HttpResponse, put, web};
 use common::api::{ApiError, ApiResponse, ApiResult};
 
 // 在路由中添加
-#[get("/task/reload")]
+#[put("/task/reload")]
 async fn task_reload(app_state: web::Data<AppState>) -> ApiResult {
     match app_state.task_manager.refresh_config().await {
         Ok(()) => Ok(HttpResponse::Ok().json(ApiResponse::ok(serde_json::json!(
