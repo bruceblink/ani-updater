1199bd0b0a635bf98664e9f20001af06dc6e9fab
likanug <likanug.g@qq.com>
2026-01-09 14:29:15 +0800

    feat: 更新"/api/me"的响应报文

--- web_services/src/routes/api/me.rs
+++ web_services/src/routes/api/me.rs
@@ -3,13 +3,28 @@ use actix_web::{HttpRequest, HttpResponse, get};
 use common::api::{ApiError, ApiResponse};
 use common::po::ApiResult;
 use common::utils::verify_jwt;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct UserView {
+    id: i64,
+    username: String,
+    roles: Vec<String>,
+    permissions: Vec<String>,
+}
 
 #[get("/me")]
 async fn me(req: HttpRequest) -> ApiResult {
     if let Some(token) = req.get_access_token()
         && let Ok(claims) = verify_jwt(&token)
     {
-        return Ok(HttpResponse::Ok().json(ApiResponse::ok(claims)));
+        let user = UserView {
+            id: claims.uid,
+            username: claims.sub,
+            roles: claims.roles,
+            permissions: vec![],
+        };
+        return Ok(HttpResponse::Ok().json(ApiResponse::ok(user)));
     }
     Err(ApiError::Unauthorized("未携带或非法的 JWT".into()))
 }
