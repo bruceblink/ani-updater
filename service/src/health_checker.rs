258201cc037a31261cb26f7ff709598a5ecf2dde
likanug <likanug.g@qq.com>
2026-01-12 17:23:26 +0800

    refactor: enhance log output readability in service/health_checker.rs

--- service/src/health_checker.rs
+++ service/src/health_checker.rs
@@ -30,8 +30,8 @@ pub async fn health_check(args: String) -> Result<ApiResponse<ItemResult>, Strin
             Ok(Ok(item)) => {
                 all_items.insert(TaskItem::Health(item));
             }
-            Ok(Err(e)) => warn!("获取检测的url失败: {}", e),
-            Err(e) => error!("任务执行失败: {}", e),
+            Ok(Err(e)) => warn!("获取检测的url失败 {}", e),
+            Err(e) => error!("任务执行失败 {}", e),
         }
     }
     result.insert(weekday, all_items);
@@ -44,17 +44,17 @@ async fn health_check_single(client: &reqwest::Client, arg: &str) -> Result<Heal
         .header("Referer", arg)
         .send()
         .await
-        .with_context(|| format!("请求待检测url失败: {}", arg))?;
+        .with_context(|| format!("请求待检测url {arg} 失败"))?;
 
     // 检查HTTP状态码
     if !response.status().is_success() {
-        anyhow::bail!("HTTP错误: {} - 状态码: {}", arg, response.status());
+        anyhow::bail!("请求 {arg} 错误- 状态码: {}", response.status());
     }
     // 将响应解析成json
     let json_value: serde_json::Value = response
         .json()
         .await
-        .with_context(|| format!("解析JSON响应失败: {}", arg))?;
+        .with_context(|| format!("解析 {arg} 的JSON响应失败: "))?;
 
     Ok(HealthItem {
         url: arg.to_string(),
@@ -68,7 +68,7 @@ mod test {
 
     #[tokio::test]
     async fn test_fetch_douban_image() {
-        let args = "http://127.0.0.1:8000/health, http://127.0.0.1:8000/health";
+        let args = "https://agileboot-back-end.onrender.com, https://agileboot-back-end.onrender.com/getConfig";
         let result = health_check(args.to_string()).await.unwrap();
         println!("{:?}", result.data)
     }
